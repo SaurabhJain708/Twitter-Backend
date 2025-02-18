2eb58b2e11300f253dcba81dabcf67599d90981a
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-02-18 19:05:29 +0530

    Added AsyncHandler

--- src/utils/AsyncHandler.ts
+++ src/utils/AsyncHandler.ts
@@ -1,1 +1,5 @@
-// keep
+export const AsyncHandler = (reqHandler:Function)=>{
+    return (req:any,res:any,next:any)=>{
+        Promise.resolve(reqHandler(req,res,next)).catch((err)=>next(err))
+    }
+} 

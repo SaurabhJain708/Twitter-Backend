d8f3009e9ef153139570bf22b4f2bfc6773a0786
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-02-20 21:17:50 +0530

    Fixed app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-export const {app}
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
 import dotenv from "dotenv";
-dotenv.config();
-
-import express from "express";
+dotenv.config({
+  path:"./.env"
+});
+import app  from "./app";
 import connectdb from "./db";
-
-const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-connectdb().then(()=>{
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+connectdb()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongoose connection failed", err);
   });
-  
-}
-
-).catch((err)=>{
-  console.log("Mongoose connection failed", err)
-})
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});

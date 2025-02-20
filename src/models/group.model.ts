f58bc30955da55134a5658bd96910d7719b2cf8d
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-02-20 20:05:41 +0530

    Completed group model

--- src/models/group.model.ts
+++ src/models/group.model.ts
@@ -1,1 +1,32 @@
-// keep
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema({
+  groupName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  admins: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+},{timestamps:true});
+
+groupSchema.virtual("noOfUsers").get(function(){
+    return this.users.length;
+})
+
+export const Group = mongoose.model("Group",groupSchema)

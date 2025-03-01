f6491eda3dfacc30f6fae5b18e10e2d4235fa020
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-03-01 21:42:44 +0530

    Added subscribe controller

---
+++ src/controllers/followers.controllers.ts
@@ -0,0 +1,34 @@
+import { Subscriber } from "../models/subscriber.model";
+import User from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { AsyncHandler } from "../utils/AsyncHandler";
+import { Request, Response } from "express";
+
+interface AuthRequest extends Request{
+    user?:any;
+}
+export const subscribe = AsyncHandler(async(req:AuthRequest,res:Response)=>{
+    if(!req?.user){
+        throw new ApiError(404,"User does not exist")
+    }
+    const {subscriberAccount,subscribedToAccount} = req.body
+    if(!subscriberAccount || !subscribedToAccount){
+        throw new ApiError(400,"Missing information")
+    }
+    const checksubscriber=  await User.findById(subscriberAccount)
+    const checksubscribedTo=  await User.findById(subscribedToAccount)
+
+    if(!checksubscriber || !checksubscribedTo){
+        throw new ApiError(400,"Please provide valid user ids")
+    }
+    const checksubexist = await Subscriber.findOne({subscriber_id:checksubscriber._id,subscribedTo_id:checksubscribedTo._id})
+    if(checksubexist){
+        throw new ApiError(400,"user is already subscribed to this account")
+    }
+    const createSubscription = await Subscriber.create({subscriber_id:checksubscriber._id,subscribedTo_id:checksubscribedTo._id})
+    if(!createSubscription){
+        throw new ApiError(500,"Something went wrong while subscribing")
+    }
+    return res.status(200).json(new ApiResponse(200,null,"Subscribed successfully"))
+})
